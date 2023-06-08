eed494430923837825cf7ad57d1eff67ee6c8491
a27k1 <xysha17@gmail.com>
2023-06-08 16:54:19 +0300

    Второе домашнее задание

---
+++ Task_2/src/main/java/Task_2/Main.java
@@ -0,0 +1,17 @@
+package Task_2;
+
+import java.util.List;
+import static Task_2.Animal.mapToProductDto;
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+        Pet cat = new Pet("Барсик", Status.AVAILABLE,
+                List.of(new Photo("Барсик с цветочком",
+                        "https://placekitten.com/200/300"),
+                        new Photo("Барсик на ручках",
+                                "https://placekitten.com/500/605")
+                ));
+
+        Animal animalCat = (Animal) mapToProductDto(cat);
+        System.out.println(animalCat);
+    }
+}
