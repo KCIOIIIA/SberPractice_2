fcd8b043311e05a78dcabf67103dccc6185fc554
a27k1 <xysha17@gmail.com>
2023-06-14 21:31:01 +0300

    Третье домашнее задание

--- Task_3/src/main/java/TimeServlet.java
+++ Task_3/src/main/java/TimeServlet.java
@@ -1,20 +1,46 @@
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeServlet extends HttpServlet {
-    @Override
-    protected  void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        Date date = new Date();
-        SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
-        writer.println(formater.format(date));
-        //writer.println("Hi");
+
+    private static Context context;
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        String contextPath = String.valueOf('/');
+        String docBase = new File(".").getAbsolutePath();
+        Context context = tomcat.addContext(contextPath, docBase);
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                    throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                Date date = new Date();
+                SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss");
+                writer.println(formater.format(date));
+            }
+        };
+        String servletName = "Servlet1";
+        String urlPattern = "/time";
+
+        tomcat.addServlet(contextPath, servletName, String.valueOf(servlet));
+        context.addServletMappingDecoded(urlPattern, servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
