d80b74e2e1f6efb113a26d9132f0c7c014220a7a
a27k1 <xysha17@gmail.com>
2023-06-22 20:27:11 +0300

    Пятое домашнее задание

--- Task_5/src/main/java/com/example/controller/MusicController.java
+++ Task_5/src/main/java/com/example/controller/MusicController.java
@@ -14,36 +14,39 @@ import java.util.List;
 @RequestMapping("/groups")
 public class MusicController {
     public List<Group> groups = new ArrayList<>();
+    public long g = 0;
 
     //Показать все группы
     @GetMapping("/")
     public Group reqGroup(Group group) {
+        group.id = g;
+        g++;
         System.out.println(group.id + "   " + group.name);
         groups.add(group);
         return group;
     }
 
-    //Показать все альбомы группы X
-    //@GetMapping("/@{group.Id}/albums")
-   // @GetMapping("/@{num}/albums")
-  //  public Album reqAlbum(@RequestParam Long num, Album album) {
-    @GetMapping("/X/albums")
-    public Album reqAlbum(Album album) {
-        System.out.println(album.id + "   " + album.name + "   " + album.year);
-       // for (int i = 0; i < groups.size(); i++){
-      //      if (groups.get(i).id.equals(num)){
-      //          groups.get(i).album.add(album);
-     //       }
-      //  }
+    //Показать все альбомы группы id0
+    @GetMapping("/{id0}/albums")
+    public Album reqAlbum(@PathVariable long id0, Album album) {
+        album.id = g;
+        g++;
+        System.out.println(album.id + "   " + album.name + "   " + album.year + "   " + album.tracks);
+        Group group = new Group();
+        group.album.add(album);
         return album;
     }
 
-    //Показать все треки альбома Y группы X
-    //@GetMapping("/@{group.Id}/albums/@{album.Id}/tracks")
-    @GetMapping("/X/albums/Y/tracks")
-    public Track reqTrack(Track track) {
+    //Показать все треки альбома id1 группы id0
+    @GetMapping("/{id0}/albums/{id1}/tracks")
+    public Track reqTrack(@PathVariable long id0, @PathVariable long id1, Track track) {
+        track.id = g;
+        g++;
         System.out.println(track.id + "   " + track.name + "   " + track.duration);
-        //groups.get(X).albums.get(Y).tracks.add(track);
+        Album album = new Album();
+        album.tracks.add(track);
+        Group group = new Group();
+        group.album.add(album);
         return track;
     }
 
